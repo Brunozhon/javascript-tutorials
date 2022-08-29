39232ce039ac4334871f8ac2987db52167eec289
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2022-08-29 16:56:13 -0400

    Update greeting-api.js

--- real-life-apps/personalized-greetings/greeting-api.js
+++ real-life-apps/personalized-greetings/greeting-api.js
@@ -1,4 +1,4 @@
 function getNameFromSearch(search) {
   var params = new URLSearchParams(search)
-  return params.get("firstName") ?? params.get("givenName") ?? params.get("name") ?? "John"
+  return params.get("firstName") ?? params.get("givenName") ?? params.get("name")
 }
